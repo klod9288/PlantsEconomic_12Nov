d62d3f82a358bbbf3ddfdfa5c86ed592a89f3a71
srongklod <klod9288@gmail.com>
2018-03-29 09:30:34 +0700

    Check My Selfe Ok.

--- app/src/main/java/srongklod_bangtamruat/plantseconomic/fragment/AddFriendCustomerFragment.java
+++ app/src/main/java/srongklod_bangtamruat/plantseconomic/fragment/AddFriendCustomerFragment.java
@@ -1,15 +1,21 @@
 package srongklod_bangtamruat.plantseconomic.fragment;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,6 +24,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import java.sql.Array;
 import java.util.ArrayList;
@@ -25,9 +32,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import srongklod_bangtamruat.plantseconomic.R;
 import srongklod_bangtamruat.plantseconomic.utility.AddFriendAdapter;
 import srongklod_bangtamruat.plantseconomic.utility.CustomerModel;
+import srongklod_bangtamruat.plantseconomic.utility.MyAlert;
+import srongklod_bangtamruat.plantseconomic.utility.MyChangeArrayListToArray;
 
 /**
  * Created by Administrator on 11/2/2561.
@@ -59,9 +69,10 @@ public class AddFriendCustomerFragment extends Fragment{
                 int timeAInt = (int) dataSnapshot.getChildrenCount();
 
                 Log.d("28MarchV1", "timeAInt ==>" + timeAInt);
-                String[] nameStrings = new String[timeAInt];
-                String[] surNameStrings = new String[timeAInt];
-                String[] imageStrings = new String[timeAInt];
+                final String[] nameStrings = new String[timeAInt];
+                final String[] surNameStrings = new String[timeAInt];
+                final String[] imageStrings = new String[timeAInt];
+                final String[] uidFriendStrings = new String[timeAInt];
 
                 ArrayList<String> stringArrayList = new ArrayList<>();
                 List list = new ArrayList();
@@ -78,6 +89,7 @@ public class AddFriendCustomerFragment extends Fragment{
                     nameStrings[timeInts[0] ]= customerModel1.getNameString();
                     surNameStrings[timeInts[0]] = customerModel1.getLastNameString();
                     imageStrings[timeInts[0]] = customerModel1.getUrlImageString();
+                    uidFriendStrings[timeInts[0]] = customerModel1.getUidUserString();
 
                     timeInts[0] += 1;
 
@@ -87,6 +99,21 @@ public class AddFriendCustomerFragment extends Fragment{
                 AddFriendAdapter addFriendAdapter = new AddFriendAdapter(getActivity(),
                         nameStrings, surNameStrings, imageStrings);
                 listView.setAdapter(addFriendAdapter);
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                        Log.d("29MarchV1", "Click Name ==>" + nameStrings[position]);
+                        confirmAddFriend(
+                                nameStrings[position],
+                                surNameStrings[position],
+                                imageStrings[position],
+                                uidFriendStrings[position],
+                                "Customer");
+
+                    }
+                });
 
 
             }//OnDataChange
@@ -98,6 +125,79 @@ public class AddFriendCustomerFragment extends Fragment{
         });
 
     }
+
+    private void confirmAddFriend(String nameString,
+                                  String surNameString,
+                                  String imageString,
+                                  final String uidFriendString,
+                                  String positionString) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.ic_action_add_friend);
+        builder.setTitle("Confirm Add Friend");
+
+        LayoutInflater layoutInflater = getActivity().getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.alert_confirm_frind, null);
+        builder.setView(view);
+
+        TextView nameTextView = view.findViewById(R.id.txtName);
+        TextView surTextView = view.findViewById(R.id.txtSurName);
+        TextView positionTextView = view.findViewById(R.id.txtPosition);
+        CircleImageView circleImageView = view.findViewById(R.id.imvAvata);
+
+        nameTextView.setText(nameString);
+        surTextView.setText(surNameString);
+        positionTextView.setText(positionString);
+
+        Picasso.with(getActivity()).load(imageString).into(circleImageView);
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                dialog.dismiss();
+
+            }
+        });
+        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                updateFirendToFirebase(uidFriendString);
+
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+
+    }//Confirm Add Friend
+
+    private void updateFirendToFirebase(String uidFriendString) {
+
+//        Find uid Current Login
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("LoginFile", Context.MODE_PRIVATE);
+        String resultFormSharePreferenc = sharedPreferences.getString("Login", null);
+        Log.d("29MarchV2", "Result" + resultFormSharePreferenc);
+
+        MyChangeArrayListToArray myChangeArrayListToArray = new MyChangeArrayListToArray(getActivity());
+        String[] loginStrings = myChangeArrayListToArray.myArray(resultFormSharePreferenc);
+        Log.d("29MarchV2", "uid Logined ==> " + loginStrings[3]);
+
+        if (uidFriendString.equals(loginStrings[3])) {
+//            Choose mySalfe
+            MyAlert myAlert = new MyAlert(getActivity());
+            myAlert.nomalDialog("Choose My Salfe","Please Choose Another Friend");
+
+
+        } else {
+
+
+        }
+
+
+    }//update friend
 
     @Nullable
     @Override
